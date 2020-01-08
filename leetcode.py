7623e9e78967c868d438e31713325385b8b3ebba
Near <943272448@qq.com>
2020-01-08 21:36:20 +0800

    [Proj]: leetcode [Type]: add [Issue]: #0002 [Detail]: https://leetcode-cn.com/problems/add-two-numbers/solution/liang-shu-xiang-jia-by-neablister/

--- leetcode.py
+++ leetcode.py
@@ -1,4 +1,5 @@
 # -*- coding:utf-8 -*-
+from utils.data_structure import *
 
 '''
 Issue: #0001
@@ -39,3 +40,57 @@ class solution_0001(object):
                     else:
                         return nums.index(n), nums.index(complement)
         return None
+
+'''
+Issue: #0002
+You are given two non-empty linked lists representing two non-negative integers.
+
+The digits are stored in reverse order and each of their nodes contain a single digit.
+
+Add the two numbers and return it as a linked list.
+
+You may assume the two numbers do not contain any leading zero, except the number 0 itself.abs
+Example:
+
+Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+Output: 7 -> 0 -> 8
+Explanation: 342 + 465 = 807.
+
+source: LeetCode
+link: https://leetcode-cn.com/problems/add-two-numbers
+'''
+class solution_0002(object):
+    def add_two_numbers(self, l1, l2):
+        '''
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        '''
+        carry = 0
+        node = ListNode(0)
+        curr = node
+        while l1 is not None or l2 is not None:
+            if l1 is not None:
+                x = l1.val
+                l1 = l1.next
+            else:
+                x = 0
+            if l2 is not None:
+                y = l2.val
+                l2 = l2.next
+            else:
+                y = 0
+            z = x + y + carry
+            curr.next = ListNode(z % 10)
+            curr = curr.next
+            if z / 10 >= 1:
+                carry = 1
+            else:
+                carry = 0
+        if carry == 1:
+            curr.next = ListNode(carry)
+        return node.next
+
+
+if __name__ == "__main__":
+    pass
