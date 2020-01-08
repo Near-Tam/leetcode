bafbacc7e5e3103ed2c71f8503afa72aaaa02801
Near <943272448@qq.com>
2020-01-08 22:15:11 +0800

    [Proj]: leetcode [Type]: add [Issue]: #0004 [Detail]: https://leetcode-cn.com/problems/median-of-two-sorted-arrays/solution/liang-ge-you-xu-shu-zu-de-zhong-wei-shu-by-neablis/

--- leetcode.py
+++ leetcode.py
@@ -91,6 +91,46 @@ class solution_0002(object):
             curr.next = ListNode(carry)
         return node.next
 
+'''
+Issue: #0004
+There are two sorted arrays nums1 and nums2 of size m and n respectively.
+
+Find the median of the two sorted arrays. The overall run time complexity should be O(log (m+n)).
+
+You may assume nums1 and nums2 cannot be both empty.
+
+Example 1:
+
+    nums1 = [1, 3]
+    nums2 = [2]
+
+    The median is 2.0
+    Example 2:
+
+    nums1 = [1, 2]
+    nums2 = [3, 4]
+
+    The median is (2 + 3)/2 = 2.5
+
+source: leetcode
+link: https://leetcode-cn.com/problems/median-of-two-sorted-arrays
+'''
+class solution_0004(object):
+    def find_median_sorted_arrays(self, nums1, nums2):
+        '''
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: float
+        '''
+        nums = nums1 + nums2
+        nums.sort()
+        l = len(nums)
+        if l % 2 == 1:
+            return nums[l/2]
+        else:
+            left = nums[l/2 - 1]
+            right = nums[l/2]
+            return (left + right) / 2.0
 
 if __name__ == "__main__":
     pass
