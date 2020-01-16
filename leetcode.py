66d04cc3b66be0f52c80eae7055f88bca02ac12e
Near <943272448@qq.com>
2020-01-16 21:58:57 +0800

    [Proj]: leetcode [Type]: add [Issue]: #0575 [Detail]: https://leetcode-cn.com/problems/distribute-candies/solution/575-fen-tang-guo-yong-shi-zui-shao-by-neablister/

--- leetcode.py
+++ leetcode.py
@@ -261,5 +261,47 @@ class solution_0009(object):
                     return False
             return True
 
+'''
+Issue: #0575
+
+Given an integer array with even length, where different numbers in this array represent different kinds of candies. Each number means one candy of the corresponding kind. You need to distribute these candies equally in number to brother and sister. Return the maximum number of kinds of candies the sister could gain.
+
+Example 1:
+
+Input: candies = [1,1,2,2,3,3]
+
+Output: 3
+
+Explanation:
+    There are three different kinds of candies (1, 2 and 3), and two candies for each kind.
+    Optimal distribution: The sister has candies [1,2,3] and the brother has candies [1,2,3], too.
+    The sister has three different kinds of candies.
+
+Example 2:
+
+Input: candies = [1,1,2,3]
+
+Output: 2
+
+Explanation:
+    For example, the sister has candies [2,3] and the brother has candies [1,1].
+    The sister has two different kinds of candies, the brother has only one kind of candies.
+
+source: leetcode
+https://leetcode-cn.com/problems/distribute-candies
+'''
+class solution_0575(object):
+    def distribute_candies(self, candies):
+        '''
+        :type candies: List[int]
+        :rtype: int
+        '''
+        l = len(candies) / 2
+        n = len(list(set(candies)))
+        if l < n:
+            return l
+        else:
+            return n
+
 if __name__ == "__main__":
     pass
